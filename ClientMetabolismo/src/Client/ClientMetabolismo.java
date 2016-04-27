3448c785ef56d0d0cbf447dae956282bb2b65799
opicimarco <difficile.opi@gmail.com>
2016-04-27 12:00:55 +0200

    ins pers e giornata

--- ClientMetabolismo/src/Client/ClientMetabolismo.java
+++ ClientMetabolismo/src/Client/ClientMetabolismo.java
@@ -17,6 +17,24 @@ public class ClientMetabolismo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    
+    inserisciPersona("opici","marco",77);
+    inserisciGiornata(27,4,2016);
+    
     }
+
+    private static void inserisciGiornata(int arg0, int arg1, int arg2) {
+        metafunction.FunzioniService service = new metafunction.FunzioniService();
+        metafunction.Funzioni port = service.getFunzioniPort();
+        port.inserisciGiornata(arg0, arg1, arg2);
+    }
+
+    private static void inserisciPersona(java.lang.String arg0, java.lang.String arg1, float arg2) {
+        metafunction.FunzioniService service = new metafunction.FunzioniService();
+        metafunction.Funzioni port = service.getFunzioniPort();
+        port.inserisciPersona(arg0, arg1, arg2);
+    }
+    
+    
     
 }
