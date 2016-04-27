4be89251b60e457312342dda858b8de2d82456f1
opicimarco <difficile.opi@gmail.com>
2016-04-27 12:18:17 +0200

    inser attivita

--- ClientMetabolismo/src/Client/ClientMetabolismo.java
+++ ClientMetabolismo/src/Client/ClientMetabolismo.java
@@ -18,8 +18,7 @@ public class ClientMetabolismo {
     public static void main(String[] args) {
         // TODO code application logic here
     
-    inserisciPersona("opici","marco",77);
-    inserisciGiornata(27,4,2016);
+    inserisciAttivita(1,1,1,1,1,1,1);
     
     }
 
@@ -33,6 +32,12 @@ public class ClientMetabolismo {
         metafunction.FunzioniService service = new metafunction.FunzioniService();
         metafunction.Funzioni port = service.getFunzioniPort();
         port.inserisciPersona(arg0, arg1, arg2);
+    }
+
+    private static void inserisciAttivita(float arg0, float arg1, float arg2, float arg3, float arg4, float arg5, float arg6) {
+        metafunction.FunzioniService service = new metafunction.FunzioniService();
+        metafunction.Funzioni port = service.getFunzioniPort();
+        port.inserisciAttivita(arg0, arg1, arg2, arg3, arg4, arg5, arg6);
     }
     
     
--- Metabolismo/src/java/MetaFunction/Funzioni.java
+++ Metabolismo/src/java/MetaFunction/Funzioni.java
@@ -62,4 +62,24 @@ public class Funzioni {
 }
     
     
+    @WebMethod(operationName="inserisciAttivita")
+    public void inserisciAttivita(float ccosciainiz,float ccosciafine, float cavaniniz,float cavanfine,float pesofinale,float durata,float pesoiniziale){
+        try{
+            System.out.println("Import driver");
+            String driver="org.apache.derby.jdbc.ClientDriver";
+            Class.forName(driver); 
+            try{
+                System.out.println("Connessione db");
+                Connection con= DriverManager.getConnection("jdbc:derby://localhost:1527/Metabolismo");
+                Statement sta= con.createStatement();
+                System.out.println("Inserisco Attivita");
+                sta.executeUpdate("insert into ATTIVITA (ccosciainiz,ccosciafine,cavaniniz,cavanfine,pesofinale,durata,pesoiniziale)"
+                        + "values ("+ccosciainiz+","+ccosciafine+","+cavaniniz+","+cavanfine+","+pesofinale+","+durata+","+pesoiniziale+")");
+            }catch(SQLException SQLExc){SQLExc.printStackTrace();}
+                        }catch(Exception e){e.printStackTrace();}
+            
+    
+}
+    
+    
 }
